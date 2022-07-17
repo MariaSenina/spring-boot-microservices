ab2a14e09a0c2fc67d4f9948c3500a5808e42a46
Maria <maria.senina15@gmail.com>
2022-07-16 21:12:18 -0400

    TwitterApp: Initialize project

---
+++ twitterapp/src/main/java/org/ac/cst8277/senina/maria/twitterapp/TwitterappApplication.java
@@ -0,0 +1,13 @@
+package org.ac.cst8277.senina.maria.twitterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterappApplication.class, args);
+	}
+
+}
---
+++ twitterapp/src/test/java/org/ac/cst8277/senina/maria/twitterapp/TwitterappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ac.cst8277.senina.maria.twitterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
