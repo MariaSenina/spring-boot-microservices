73f2a71b5a5098205844e5949a1d181fe8e02aad
Maria <maria.senina15@gmail.com>
2022-07-17 15:04:51 -0400

    UserManagementService: Rename TwitterApp to UserManagementService

--- user-management-service/src/main/java/org/ac/cst8277/senina/maria/twitterapp/UserManagementServiceApplication.java
+++ user-management-service/src/main/java/org/ac/cst8277/senina/maria/twitterapp/UserManagementServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TwitterappApplication {
+public class UserManagementServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TwitterappApplication.class, args);
+		SpringApplication.run(UserManagementServiceApplication.class, args);
 	}
 
 }
