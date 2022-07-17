e0e5dc38e0afff505061cdd02150cc473274f3dd
Maria <maria.senina15@gmail.com>
2022-07-17 00:39:12 -0400

    TwitterApp: Add basic user home page

--- twitterapp/src/main/java/org/ac/cst8277/senina/maria/twitterapp/controllers/LoginController.java
+++ twitterapp/src/main/java/org/ac/cst8277/senina/maria/twitterapp/controllers/LoginController.java
@@ -1,9 +1,11 @@
 package org.ac.cst8277.senina.maria.twitterapp.controllers;
 
 import org.ac.cst8277.senina.maria.twitterapp.dtos.LoginRequestDto;
+import org.ac.cst8277.senina.maria.twitterapp.entities.User;
 import org.ac.cst8277.senina.maria.twitterapp.services.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,8 +26,10 @@ public class LoginController {
     }
 
     @PostMapping
-    public String doLogin(LoginRequestDto requestDto) {
-        System.out.println(loginService.findByEmailAndPassword(requestDto.getEmail(), requestDto.getPassword()));
-        return "login";
+    public String doLogin(LoginRequestDto requestDto, Model model) {
+        User user = loginService.findByEmailAndPassword(requestDto.getEmail(), requestDto.getPassword());
+        model.addAttribute("user", user);
+
+        return "userHome";
     }
 }
